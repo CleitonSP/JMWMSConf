75b03fe7d8aa87ac42b419e7ddaff8c4ed176b0c
Davi Igor Giles de Souza <davi@onofree.com.br>
2013-10-31 11:05:11 -0200

    ok

---
+++ AcessoDados/ArquivoTexto.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using System.IO;
+
+namespace AcessoDados
+{
+    public class ArquivoTexto
+    {
+        
+        public String LerArquivoTexto(string strCaminhoNomeArquivo)
+        {
+            try
+            {
+                return System.IO.File.ReadAllText(@"" + strCaminhoNomeArquivo);
+
+            }
+            catch (System.IO.IOException Erro)
+            {
+                return "Erro : " + Erro.Message;
+            }
+        }
+
+        public String[] LerLinhasTexto(string strCaminhoNomeArquivo)
+        {
+            try
+            {
+                return System.IO.File.ReadAllLines(@"" + strCaminhoNomeArquivo);
+
+            }
+            catch (System.IO.IOException Erro)
+            {
+                return new string[] { "Erro : " + Erro.Message };
+            }
+        }
+
+        public byte[] LerArquivoBytes(string strCaminhoNomeArquivo)
+        {
+            try
+            {
+                return
+                System.IO.File.ReadAllBytes(@"" + strCaminhoNomeArquivo);
+
+            }
+            catch (Exception Erro)
+            {
+                Console.WriteLine("Erro : " + Erro.Message);
+                return null;
+            }
+
+        }
+
+        public Boolean EscreverArquivoXml(string strCaminhoNomeArquivo, XmlDocument xmlConteudo)
+        {
+            try
+            {
+                xmlConteudo.Save(@"" + strCaminhoNomeArquivo + ".xml");
+                return true;
+            }
+            catch (System.IO.IOException Erro)
+            {
+                Console.WriteLine("Erro : " + Erro.Message);
+                return false;
+            }
+        }
+
+        public Boolean EscreverArquivoTexto(string strCaminhoNomeArquivo, string strTexto)
+        {
+            try
+            {
+                //escreve todo o texto no caminho especificado
+                System.IO.File.WriteAllText(@"" + strCaminhoNomeArquivo, strTexto);
+
+                return true;
+            }
+            catch (System.IO.IOException Erro)
+            {
+                Console.WriteLine("Erro : " + Erro.Message);
+                return false;
+            }
+
+        }
+
+        public Boolean ConcatenaArquivoTexto(string strCaminhoNomeArquivo, string strTexto)
+        {
+            try
+            {
+                //concatena ao arquivo informado todo o texto recebido como parametro
+                System.IO.File.AppendAllText(@"" + strCaminhoNomeArquivo, strTexto);
+
+                return true;
+            }
+            catch (System.IO.IOException Erro)
+            {
+                Console.WriteLine("Erro : " + Erro.Message);
+                return false;
+            }
+        }
+
+        public Boolean EscreverArquivoBytes(string strCaminhoNomeArquivo, byte[] btBytes)
+        {
+            try
+            {
+                System.IO.File.WriteAllBytes(@"" + strCaminhoNomeArquivo , btBytes);
+                return true;
+            }
+            catch (Exception Erro)
+            {
+                Console.WriteLine("Erro : " + Erro.Message);
+                return false;
+            }
+
+        }
+
+        public Boolean DeletarArquivoTexto(string NomeCaminho)
+        {
+            try
+            {
+                FileInfo fiArqquivo = new FileInfo(NomeCaminho);
+
+                fiArqquivo.Delete();
+
+                return true;
+            }
+            catch (Exception Erro)
+            {
+                Console.WriteLine("Erro : " + Erro.Message);
+                return false;
+            }
+
+        }
+
+    }
+    }
+}
---
+++ AcessoDados/ConexaoJmoreira.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SqlClient;
+using AcessoDados.Properties;
+using System.Data;
+
+namespace AcessoDados
+{
+    public class ConexaoJmoreira
+    {
+
+        #region Pegar Conexão
+        public SqlConnection PegarConexao()
+        {
+            try
+            {
+
+                return new SqlConnection(Settings.Default.Jmoreira);
+
+
+            }
+            catch (Exception Erro)
+            {
+                throw new Exception(Erro.Message);
+            }
+        }
+        #endregion
+
+        #region Parametros
+
+        private SqlParameterCollection objparametros = new SqlCommand().Parameters;
+
+        public void LimparParametros()
+        {
+            try
+            {
+                objparametros.Clear();
+            }
+            catch (Exception erro)
+            {
+                throw new Exception("Erro ao Limpar paramêtro. Messagem:" + erro.Message);
+            }
+        }
+
+        public void AddParametros(string strNomeParametro, object objValorparametro)
+        {
+            try
+            {
+                objparametros.Add(new SqlParameter(strNomeParametro, objValorparametro));
+            }
+            catch
+            {
+                throw new Exception("Erro ao adicionar paramêtro na camada de Acesso a Dados!");
+            }
+        }
+        #endregion
+
+        #region Persistencia, Inserir, Alterar, Excluir.
+
+        public object ExecutarManipulacao(CommandType objtipocomando, string strtextosql)
+        {
+            try
+            {
+                SqlConnection objconexao = PegarConexao();
+                objconexao.Open();
+                SqlCommand ObjComando = objconexao.CreateCommand();
+
+                //ObjComando.CommandTimeout = 8000;
+
+                ObjComando.CommandType = objtipocomando;
+
+                ObjComando.CommandText = strtextosql;
+
+
+                foreach (SqlParameter objparametro in objparametros)
+                {
+                    ObjComando.Parameters.Add(new SqlParameter { ParameterName = objparametro.ParameterName, Value = objparametro.Value });
+                }
+
+                object objEscalar = ObjComando.ExecuteScalar();
+                objconexao.Close();
+                return objEscalar;
+
+            }
+            catch (Exception erro)
+            {
+                throw new Exception(erro.Message);
+            }
+        }
+        #endregion
+
+        #region Consultar
+
+        public DataTable ConsultarRegistros(CommandType objtipocomando, string strtextosql)
+        {
+            try
+            {
+                SqlConnection objConexao = PegarConexao();
+
+                objConexao.Open();
+
+                SqlCommand objcomando = objConexao.CreateCommand();
+                objcomando.CommandTimeout = 0;
+
+                objcomando.CommandType = objtipocomando;
+
+                objcomando.CommandText = strtextosql;
+
+                foreach (SqlParameter objParametro in objparametros)
+                {
+                    objcomando.Parameters.Add(new SqlParameter { ParameterName = objParametro.ParameterName, Value = objParametro.Value });
+                }
+
+                SqlDataAdapter objadaptador = new SqlDataAdapter(objcomando);
+
+                DataTable objTabela = new DataTable();
+
+                objadaptador.Fill(objTabela);
+
+
+                //fecha a conexão
+                objConexao.Close();
+
+                return objTabela;
+            }
+            catch (Exception erro)
+            {
+                throw new Exception(erro.Message);
+            }
+        }
+        #endregion
+
+        #region INSERE NO BANCO UM DATATABLE COMPLETO SEM FOREACH COM A FUNÇÃO BULKCOPY
+        public Boolean TransfereBancoJmoreira(DataTable dtOrigem, String strTblDestino)
+        {
+            SqlConnection Conexao = PegarConexao();
+            Conexao.Open();
+
+            using (SqlBulkCopy bcCarregar = new SqlBulkCopy(Conexao))
+            {
+                bcCarregar.DestinationTableName =
+                    strTblDestino;
+
+                try
+                {
+                    // Write from the source to the destination.
+                    bcCarregar.WriteToServer(dtOrigem);
+
+                    return true;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    return false;
+                }
+                finally
+                {
+                    Conexao.Close();
+                }
+            }
+
+
+        }
+        #endregion
+
+
+    }
+}
